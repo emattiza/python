5985c4699b11be7b3de2b0b1d1d67a36f55d5671
emattiza <emattiza@gmail.com>
2023-02-13 11:30:14 -0600

    add images to flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,57 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix2container.url = "github:nlewo/nix2container";
   };
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    nix2container,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        containerPkgs = nix2container.packages.${system};
         python-pdal = pkgs.callPackage ./default.nix {inherit pkgs;};
+        interpreter = pkgs.python310.withPackages (ps: [python-pdal]);
+        containerBuildProd = containerPkgs.nix2container.buildImage {
+          name = "python-pdal";
+          tag = "prod";
+          layers = [
+            (containerPkgs.nix2container.buildLayer
+              {
+                deps = [interpreter];
+              })
+          ];
+          config = {
+            entrypoint = ["${interpreter}/bin/python" "-i"];
+          };
+        };
+        containerBuild = containerPkgs.nix2container.buildImage {
+          name = "python-pdal";
+          tag = "dev";
+          copyToRoot = pkgs.buildEnv {
+            name = "root";
+            paths = [pkgs.bashInteractive pkgs.coreutils pkgs.ripgrep pkgs.silver-searcher];
+            pathsToLink = ["/bin"];
+          };
+          layers = [
+            (containerPkgs.nix2container.buildLayer
+              {
+                deps = [interpreter];
+              })
+          ];
+          config = {
+            entrypoint = ["${interpreter}/bin/python" "-i"];
+          };
+        };
       in {
         packages = flake-utils.lib.flattenTree rec {
-          python-pdal = python-pdal;
+          inherit python-pdal;
+          dev-python-pdal-image = containerBuild;
+          prod-python-pdal-image = containerBuildProd;
           default = python-pdal;
         };
         devShells = rec {
